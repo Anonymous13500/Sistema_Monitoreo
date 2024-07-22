7c5f3255633d2a705b95c15738f0e09ea7a64333
Angel Jose Cedeno Moreira <acedeno7097@utm.edu.ec>
2024-07-22 11:15:24 -0500

    Primer commit de prueba

---
+++ components/cards.php
@@ -0,0 +1,9 @@
+<div class="card-container">
+    <i class="fas fa-info-circle"></i>
+    <div class="card" style="background-color: <?php echo $bgcolor; ?>;">
+        <i style="font-size: 40px;"></i>
+        <h2><?php echo $title; ?></h2>
+        <p><?php echo $content; ?></p>
+    </div>
+</div>
+ 
---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema de Monitoreo</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <style>
+        .card-container {
+            position: relative;
+            display: inline-block;
+            margin: 10px;
+        }
+
+        .card-container i {
+            font-size: 50px;
+            color: #000;
+            cursor: pointer;
+        }
+
+        .card {
+            display: none;
+            background-color: #e0d4f7;
+            border-radius: 15px;
+            padding: 20px;
+            width: 300px;
+            text-align: center;
+            position: absolute;
+            top: 60px;
+            left: 0;
+            z-index: 10;
+        }
+
+        .card-container:hover .card {
+            display: block;
+        }
+    </style>
+</head>
+<body>
+    <div style="display: flex; flex-wrap: wrap; justify-content: center;">
+        <?php
+        $cards = [
+            [
+                'title' => 'Frequency',
+                'content' => 'Cantidad de ciclos de una onda sinusoidal que ocurren',
+                'bgcolor' => '#e0d4f7'
+            ],
+            [
+                'title' => 'Voltage',
+                'content' => 'Diferencia de potencial eléctrico entre dos puntos',
+                'bgcolor' => '#d4f7e0'
+            ]
+        ];
+
+        foreach ($cards as $card) {
+            $title = $card['title'];
+            $content = $card['content'];
+            $bgcolor = $card['bgcolor'];
+            include './components/cards.php';
+        }
+        ?>
+    </div>
+</body>
+</html>
